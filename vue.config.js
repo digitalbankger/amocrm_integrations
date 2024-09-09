90a1151e820eea3b74f9f8f05eee91c9e7dad199
digitalbankger <bkmzen.bkmzen@mail.ru>
2024-09-09 15:06:13 +0300

    last

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/amocrm_integrations/'
 })
-export default {
-  base: '/amocrm_integrations/', 
-};
